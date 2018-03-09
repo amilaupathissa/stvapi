1a445c31f1ebb92fc32e1835fb6aabb36a5aead9
Amila Upathissa <amilaupathissa@gmail.com>
2018-03-09 14:49:09 +0530

    finish basic mail send

---
+++ app/Http/Controllers/Message/MessageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Message;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Message as MessageResource;
+use Mail;
+
+class MessageController extends Controller
+{
+    public function mailSend(Request $request)
+    {
+        $data = new MessageResource($request);
+        $name = $data->name;
+        $email = $data->email;
+        $text = $data->text;
+        $subject = $data->subject;
+        
+
+        return Mail::send(['text'=>'email'],['name'=>$name,"subject"=>$subject,'email'=> $email,'text'=> $text],function($message) use ($subject,$name){
+            $message->to('web.shraddha@gmail.com', 'to backend')
+                    ->subject($subject);
+            $message->from('mobile.app.shraddha@gmail.com','Message from: '.$name);
+
+        });
+    }
+    
+}
---
+++ resources/views/form.blade.php
@@ -0,0 +1,10 @@
+<lable>Name</lable>
+<input type="text">
+<br>
+<lable>Email</lable>
+<input type="text">
+<br>
+<lable>Message</lable>
+<textarea></textarea>
+<br>
+<button type="submit">submit</button>
